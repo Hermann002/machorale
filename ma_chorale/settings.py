1c1f0a97b5aa2491787317c877cc1bb468e03832
Hermann002 <hermannnzeudeu@gmail.com>
2026-03-28 20:19:41 -0400

    hum

--- ma_chorale/settings.py
+++ ma_chorale/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from decouple import config
+from django.urls import reverse_lazy
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -142,6 +143,8 @@ SESSION_COOKIE_SAMESITE = 'Lax'
 CACHE_MIDDLEWARE_SECONDS = 600
 CACHE_MIDDLEWARE_KEY_PREFIX = 'ma_chorale'
 CACHE_MIDDLEWARE_ANONYMOUS_ONLY = True
+
+LOGIN_URL = reverse_lazy('login')
 
 CACHES = {
     "default": {
