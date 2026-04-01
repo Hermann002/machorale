42d6e0fe1e8e0fbf0d15d103f7ca889c262d2b71
Hermann002 <hermannnzeudeu@gmail.com>
2026-03-31 23:14:14 -0400

    change url

--- manage_chorale/urls.py
+++ manage_chorale/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from .views import DashboardView, CreateChoraleView, ContributionView, ListMembersView, MemberPopupView, close_popup, sidebar_toggle
 
 urlpatterns = [
-    path("create-chorale/", CreateChoraleView.as_view(), name="create_chorale"),
-    path("dashboard/", DashboardView.as_view(), name="dashboard"),
-    path("list-members/", ListMembersView.as_view(), name="members"),
-    path("member-popup/", MemberPopupView.as_view(), name="member_popup"),
-    path("contributions/", ContributionView.as_view(), name="contributions"),
+    path("<slug:chorale_name>/create-chorale/", CreateChoraleView.as_view(), name="create_chorale"),
+    path("<slug:chorale_name>/dashboard/", DashboardView.as_view(), name="dashboard"),
+    path("<slug:chorale_name>/list-members/", ListMembersView.as_view(), name="members"),
+    path("<slug:chorale_name>/member-popup/", MemberPopupView.as_view(), name="member_popup"),
+    path("<slug:chorale_name>/contributions/", ContributionView.as_view(), name="contributions"),
     path("sidebar-toggle/", sidebar_toggle, name="sidebar_toggle"),
     path("close-popup/", close_popup, name="close_popup"),
 ]
