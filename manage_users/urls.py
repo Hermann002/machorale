b96c03131f464ee1031e280bba4da52c804247bf
Hermann002 <hermannnzeudeu@gmail.com>
2026-04-03 18:11:53 -0400

    hum

--- manage_users/urls.py
+++ manage_users/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("verify-email/", VerifyEmailView.as_view(), name="verify_email"),
-    # path("resend-otp/", ResendOtpView.as_view(), name="resend_otp")
+    # path("resend-otp/", ResendOtpView.as_view(), name="resend_otp"),
 ]
