7919bf58a29435e0831490ffff37d12de121d0de
Hermann002 <hermannnzeudeu@gmail.com>
2026-05-29 20:44:03 -0400

    test nouveau deploiment 5

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,13 +5,4 @@ python manage.py collectstatic --noinput
 echo "Applying database migrations..."
 python manage.py migrate --noinput
 
-echo "Running tests..."
-DJANGO_SETTINGS_MODULE=ma_chorale.settings.test pytest
-
-if [ $? -ne 0 ]; then
-    echo "Tests failed. Exiting."
-    exit 1
-fi
-
-echo "✅ All tests passed, Starting application..."
 exec "$@"
